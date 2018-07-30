ecbae8cd20e3303d6fcb393ad6abf61222a3235c
Lakjeewa <lakjeewa@Populos-Mac-mini-2.local>
2018-07-30 13:35:05 +0530

    displayed the name retrieved through the URL

---
+++ app/src/main/java/kliq/example/uri/uritestandroid/URIActivity.java
@@ -0,0 +1,22 @@
+package kliq.example.uri.uritestandroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class URIActivity extends AppCompatActivity {
+private TextView hostTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uri);
+        hostTextView = (TextView) findViewById(R.id.host);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        Uri data = intent.getData();
+        hostTextView.setText(data.getQueryParameter("name"));
+
+    }
+}
